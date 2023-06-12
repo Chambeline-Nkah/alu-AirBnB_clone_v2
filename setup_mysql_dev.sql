1c06b14a6494ba17776e7532c8cf292db925c03f
Gasorekibo <m.gasore@alustudent.com>
2023-06-12 12:33:46 +0200

    Prepare mysql server for this project

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+--- CREATING MYSQL USER AND GRANT THE PRIVILEGE TO HIM ---
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON perforamance_schema.* TO 'hbnb_dev'@'localhost';

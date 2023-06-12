bfd2819d8a6801279c180b8d2b5a262973c198c3
Gasorekibo <m.gasore@alustudent.com>
2023-06-12 22:27:06 +0200

    Starts working with sqlalchemy

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,18 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    id = (Column(String(60), unique=True, primary_key=True, nullable=False),)
+    created_at = (Column(datetime, nullable=False, default=datetime.utcnow()),)
+    updated_at = Column(datetime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
@@ -36,12 +44,21 @@ class BaseModel:
         from models import storage
 
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
+
+    def delete(self):
+        """Deletes the current instance from the storage"""
+        from models import storage
+
+        storage.delete(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
+        if "_sa_instance_state" in dictionary:
+            del dictionary["_sa_instance_state"]
         dictionary.update({"__class__": (str(type(self)).split(".")[-1]).split("'")[0]})
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()

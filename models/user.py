a9fb34321c1d74364c86db23a910e061fb61681b
Chambeline-Nkah <chambelinenkah57@gmail.com>
2023-06-13 10:47:52 +0200

    task7

--- models/user.py
+++ models/user.py
@@ -19,7 +19,4 @@ email = Column(String(128), nullable=False)
 password = Column(String(128), nullable=False)
 first_name = Column(String(128), nullable=True)
 last_name = Column(String(128), nullable=True)
-places = relationship('Place', backref='user',
-                        cascade='all, delete-orphan')
-reviews = relationship('Review', backref='user',
-                        cascade='all, delete-orphan')
+

f4b573abe8607070d79a9eb112173d0cda6309de
Gasorekibo <m.gasore@alustudent.com>
2023-06-13 17:19:29 +0200

    Working on question 6

--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,21 @@
 from models.engine.file_storage import FileStorage
 
 
-storage = FileStorage()
-storage.reload()
+# storage = FileStorage()
+# storage.reload()
+import os
+from dotenv import find_dotenv, load_dotenv
+
+env_path = find_dotenv()
+load_dotenv(env_path)
+
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+
+    storage = FileStorage()
+    storage.reload()

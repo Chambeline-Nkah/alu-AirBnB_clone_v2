c46505120c334c45dcae19707ac80e2e7399f3ca
Gasorekibo <m.gasore@alustudent.com>
2023-06-14 10:42:24 +0200

    Try Debugging

--- models/user.py
+++ models/user.py
@@ -1,24 +1,24 @@
-# #!/usr/bin/python3
-# """This module defines the User class"""
-# from models.base_model import BaseModel, Base
-# from sqlalchemy import String, Column
-# from sqlalchemy.orm import relationship
+#!/usr/bin/python3
+"""This module defines the User class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
 
 
-# class User(BaseModel, Base):
-#     """This class defines a user by various attributes:
-#     email = ''
-#     password = ''
-#     first_name = ''
-#     last_name = ''
-#     """
+class User(BaseModel, Base):
+    """This class defines a user by various attributes:
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
+    """
 
 
-# __tablename__ = "users"
+__tablename__ = "users"
 
-# email = Column(String(128), nullable=False)
-# password = Column(String(128), nullable=False)
-# first_name = Column(String(128), nullable=True)
-# last_name = Column(String(128), nullable=True)
-# places = relationship("Place", backref="user", cascade="all, delete-orphan")
-# reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
+email = Column(String(128), nullable=False)
+password = Column(String(128), nullable=False)
+first_name = Column(String(128), nullable=True)
+last_name = Column(String(128), nullable=True)
+places = relationship("Place", backref="user", cascade="all, delete-orphan")
+reviews = relationship("Review", backref="user", cascade="all, delete-orphan")

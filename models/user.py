20f9cc26c0ced1948d490d3e67a159d9dedf1afe
Gasorekibo <m.gasore@alustudent.com>
2023-06-14 14:57:18 +0200

    Try Debugging

--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines the User class"""
-from .base_model import BaseModel, Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
 

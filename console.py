5516a0adcb585187b6ca152dc9ebcae93b7ba10d
Gasorekibo <m.gasore@alustudent.com>
2023-06-14 10:35:30 +0200

    Try Debugging

--- console.py
+++ console.py
@@ -2,10 +2,11 @@
 """ Console Module """
 import cmd
 import sys
+import models
 from models.base_model import BaseModel
 from models.__init__ import storage
-from models.user import User
 from models.place import Place
+from models.user import User
 from models.state import State
 from models.city import City
 from models.amenity import Amenity

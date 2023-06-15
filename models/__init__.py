3477b897e9f35eaa5a60210d1a7c129a02382e51
Gasorekibo <m.gasore@alustudent.com>
2023-06-15 12:30:33 +0200

    Try Debugging

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-# import os
-from dotenv import find_dotenv, load_dotenv
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 import os
+from dotenv import find_dotenv, load_dotenv
+
 
 env_path = find_dotenv()
 load_dotenv(env_path)

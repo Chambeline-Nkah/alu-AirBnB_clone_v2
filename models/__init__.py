7e9b65f6e3bc1659ee3bf1461a0348fc6ca55eb5
Gasorekibo <m.gasore@alustudent.com>
2023-06-15 09:34:35 +0200

    Try Debugging

--- models/__init__.py
+++ models/__init__.py
@@ -1,19 +1,19 @@
-# #!/usr/bin/python3
-# """This module instantiates an object of class FileStorage"""
-# # import os
-# # from dotenv import find_dotenv, load_dotenv
+#!/usr/bin/python3
+"""This module instantiates an object of class FileStorage"""
 # import os
+# from dotenv import find_dotenv, load_dotenv
+import os
 
-# # env_path = find_dotenv()
-# # load_dotenv(env_path)
+# env_path = find_dotenv()
+# load_dotenv(env_path)
 
-# if os.getenv("HBNB_TYPE_STORAGE") == "db":
-#     from models.engine.db_storage import DBStorage
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
 
-#     storage = DBStorage()
-#     storage.reload()
-# else:
-#     from models.engine.file_storage import FileStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
 
-#     storage = FileStorage()
-#     storage.reload()
+    storage = FileStorage()
+    storage.reload()

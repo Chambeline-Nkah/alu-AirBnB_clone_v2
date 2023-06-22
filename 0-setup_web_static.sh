a3bb0de73b876f9156d4d840bfe83bc60d663617
Gasorekibo <m.gasore@alustudent.com>
2023-06-22 12:33:18 +0200

    Fabrics deployment, Prep web server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,43 @@
 #!/usr/bin/env bash
-# Prepare my web server to fulfill the requirement
+# Preparing my web server
 
 # Install Nginx if not already installed
-if ! [ command -v nginx &> /dev/null]; then
-    sudo apt-get update
-    sudo apt-get install -y nginx
+if ! command -v nginx >/dev/null 2>&1; then
+    apt-get update
+    apt-get -y install nginx
 fi
 
 # Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 
-# Create a fake HTML file for testing
-sudo echo "<html><head></head><body>Test Page</body></html>" | sudo tee /data/web_static/releases/test/index.html
+# Create a fake HTML file
+echo "Hello, world!" > /data/web_static/releases/test/index.html
 
-# Create symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create or recreate symbolic link
+if [ -L /data/web_static/current ]; then
+    rm /data/web_static/current
+fi
+ln -s /data/web_static/releases/test/ /data/web_static/current
 
-# Set ownership recursively
-sudo chown -R ubuntu:ubuntu /data/
+# Set ownership of the /data/ folder recursively
+chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
 config_file="/etc/nginx/sites-available/default"
-sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' "$config_file"
+config_block=$(cat <<EOF
+        location /hbnb_static {
+            alias /data/web_static/current/;
+            index index.html;
+        }
+EOF
+)
+
+# Add or update the location block in the Nginx configuration file
+if grep -q "location /hbnb_static" "$config_file"; then
+    sed -i '/location \/hbnb_static/,$d' "$config_file"
+fi
+sed -i "/^}/i $config_block" "$config_file"
 
 # Restart Nginx
-sudo service nginx restart
-
-exit 0
+service nginx restart

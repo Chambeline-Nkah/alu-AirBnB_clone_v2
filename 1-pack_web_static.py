26132ff3746eb69a9e254ead4b3624021302cbc3
Chambeline-Nkah <chambelinenkah57@gmail.com>
2023-06-22 22:52:44 +0200

    task1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Comment"""
+"""Write a Fabric script that generates a .tgz
+archive from the contents of the web_static folder"""
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
-    """Comment again"""
+    """do_pack fxn"""
     local("mkdir -p versions")
 
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")

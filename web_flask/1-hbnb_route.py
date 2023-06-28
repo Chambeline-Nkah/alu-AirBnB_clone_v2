3d9bfbe6943ff3dce692bfde7bdf8a8a208ca983
Chambeline-Nkah <chambelinenkah57@gmail.com>
2023-06-28 13:24:59 +0200

    error_output

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,10 +12,11 @@ def hello_hbnb():
     """ Displaying Hello HBNB! """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb")
 def hbnb():
     """ Display HBNB! """
-    return "HBNB!"
+    return "HBNB"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
